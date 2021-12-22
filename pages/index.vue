bc4abc4efde0bb4888174c033a9388eb02feec84
Ayomide Oluniyi <ayorg360@gmail.com>
2021-12-22 14:39:18 +0100

    chore: init repo

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <iframe
+      src="/JLDigitalMenu.pdf"
+      frameborder="0"
+      class="h-screen w-screen"
+    ></iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "Menu | Julie's Launja",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "View Julie's Launja Menu and pick from a variety delicious food options",
+        },
+      ],
+    };
+  },
+};
+</script>
